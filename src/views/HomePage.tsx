c5204c67ab3e9977c95613b48d7aa4935c61cc4b
lawrence122 <54451749+lawrence122@users.noreply.github.com>
2026-08-19 02:44:02 -0400

    added router and more details on the home page

---
+++ src/views/HomePage.tsx
@@ -0,0 +1,86 @@
+import { useState } from 'react'
+
+const HomePage = () => {
+    const [stopScroll, setStopScroll] = useState(false);
+    const cardData = [
+        {
+            title: "Unlock Your Creative Flow",
+            image: "https://images.unsplash.com/photo-1543487945-139a97f387d5?w=1200&auto=format&fit=crop&q=60",
+        },
+        {
+            title: "Design Your Digital Future",
+            image: "https://images.unsplash.com/photo-1529254479751-faeedc59e78f?w=1200&auto=format&fit=crop&q=60",
+        },
+        {
+            title: "Build with Passion, Ship with Pride",
+            image: "https://images.unsplash.com/photo-1618327907215-4e514efabd41?w=1200&auto=format&fit=crop&q=60",
+        },
+        {
+            title: "Think Big, Code Smart",
+            image: "https://images.unsplash.com/photo-1583407723467-9b2d22504831?w=1200&auto=format&fit=crop&q=60",
+        },
+    ];
+
+    return (
+        <div className='m-5'>
+            {/* My intro */}
+            <div className='grid grid-cols-2 gap-3' >
+                <div className='text-left' >
+                    <h1>Bonjour!</h1>
+                    <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets. It has survived not only many decades, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised thanks to these sheets and more recently with desktop publishing software like Aldus PageMaker and Microsoft Word including versions of Lorem Ipsum.
+                    </p>
+                </div>
+                <div className='mt-5'>
+                    <img src="https://images.unsplash.com/photo-1543487945-139a97f387d5?w=1200&auto=format&fit=crop&q=60"
+                    className="mr-3 h-60 sm:h-85 w-60 sm:w-85 rounded-lg"
+                    />
+                </div>
+            </div>
+
+            {/* Project gallery */}
+            <div className='md:flex m-5 relative' >
+                <div className='grid grid-cols-1 items-center justify-center'>
+                    <div className='animate-caret-fade m-15' >
+                        {'𓆉⋆｡˚⋆❀ 🐚🫧𓇼 ˖° Projects 🪼⋆｡𖦹°🫧⋆.ೃ࿔*:･'.split('').map((c, idx) => {
+                            const startDelay = 0
+                            const delay = startDelay + (150 * idx)
+                            const style = c === '.' ? 'text-2xl md:text-3xl lg:text-4xl' : 'text-xl md:text-2xl lg:text-3xl'
+
+                            return <span key={`k-${c}-${delay}`}
+                            style={{ animationDelay: `${delay}ms` }}
+                            className={`${style} text-fuchsia-800 animate-[bounce_1s_infinite_ease-in-out,pulse_2s_infinite_ease-in-out]`}
+                            >
+                            {c}
+                            </span>
+
+                        })}
+                    </div>
+
+                    <div className="overflow-hidden w-full relative max-w-6xl mx-auto" onMouseEnter={() => setStopScroll(true)} onMouseLeave={() => setStopScroll(false)}>
+                        <div className="absolute left-0 top-0 h-full w-20 z-10 pointer-events-none bg-linear-to-r from-white to-transparent" />
+                            <div className="marquee-inner flex w-fit" style={{ animationPlayState: stopScroll ? "paused" : "running", animationDuration: cardData.length * 2500 + "ms" }}>
+                                <div className="flex">
+                                    {[...cardData, ...cardData].map((card, index) => (
+                                        <div key={index} className="w-56 mx-4 h-80 relative group hover:scale-90 transition-all duration-300">
+                                            <img src={card.image} alt="card" className="w-full h-full object-cover" />
+                                            <div className="flex items-center justify-center px-4 opacity-0 group-hover:opacity-100 transition-all duration-300 absolute bottom-0 backdrop-blur-md left-0 w-full h-full bg-black/20">
+                                                <p className="text-white text-lg font-semibold text-center">{card.title}</p>
+                                            </div>
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        <div className="absolute right-0 top-0 h-full w-20 md:w-40 z-10 pointer-events-none bg-linear-to-l from-white to-transparent" />
+                    </div>
+
+                    <button type='button' className='bg-amber-500'>
+                        More 𐫱✿❀❁✽
+                    </button>
+                </div>
+            </div>
+        </div>
+     )
+}
+
+export default HomePage
